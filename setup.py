101125e21aa4ccc4b25a74f461ef61d3667ca559
Aceticia <liuchris34@gmail.com>
2022-05-29 16:47:58 -0400

    changing package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='text2table',
     packages=find_packages(),
     version='0.1.0',
     description='Convert texts into tables with the power of magic.',
