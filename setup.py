5f38fc02e33f4dc3746decb6530d9564ed3295d5
Aceticia <liuchris34@gmail.com>
2022-05-29 16:31:47 -0400

    adding template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Convert texts into tables with the power of magic.',
+    author='OLab',
+    license='MIT',
+)
