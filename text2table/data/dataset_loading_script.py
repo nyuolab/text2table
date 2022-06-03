10cdb0617a3ad513b104647fcb0f0cc6dd51ffaf
Yang <yangz09@bigpurple-ln2.cm.cluster>
2022-06-03 18:44:16 -0400

    dataset loading

--- text2table/data/dataset_loading_script.py
+++ text2table/data/dataset_loading_script.py
@@ -11,20 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# TODO: Address all TODOs and remove all explanatory comments
-"""TODO: Loading script for MIMIC3 dataset."""
+"""Loading script for MIMIC3 dataset."""
 
 
 import csv
-import json
 from multiprocessing.sharedctypes import Value
 import os
 
 import datasets
 
 
-# TODO: Add BibTeX citation
-# Find for instance the citation on arxiv or on the dataset repo/website
+# BibTeX citation
 _CITATION = """\
  @article{johnson_pollard_shen_lehman_feng_ghassemi_moody_szolovits_anthony celi_mark_et al._2016, 
  title={Mimic-III, a freely accessible Critical Care Database}, 
@@ -36,8 +33,7 @@ _CITATION = """\
  year={2016}} 
 """
 
-# TODO: Add description of the dataset here
-# You can copy an official description
+# description of the dataset
 _DESCRIPTION = """\
 MIMIC-III (‘Medical Information Mart for Intensive Care’) is a large, single-center database 
 comprising information relating to patients admitted to critical care units at a large tertiary care hospital. 
@@ -46,47 +42,37 @@ observations and notes charted by care providers, fluid balance, procedure codes
 diagnostic codes, imaging reports, hospital length of stay, survival data, and more.
 """
 
-# TODO: Add a link to an official homepage for the dataset here
+# a link to an official homepage for the dataset here
 _HOMEPAGE = "https://physionet.org/content/mimiciii-demo/1.4/"
 
-# TODO: Add the licence for the dataset here if you can find it
+# the licence for the dataset here if you can find it
 _LICENSE = "https://physionet.org/content/mimiciii-demo/view-license/1.4/"
 
-# TODO: Add link to the official dataset URLs here
-# The HuggingFace Datasets library doesn't host the datasets but only points to the original files.
-# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
-_URLS = {
-    "first_domain": "https://huggingface.co/great-new-dataset-first_domain.zip",
-    "second_domain": "https://huggingface.co/great-new-dataset-second_domain.zip",
-}
+# link to the official dataset URLs here
+# Since we work with our dataset locally, we don't need URLs.
+# _URLS = {
+#     "first_domain": "https://huggingface.co/great-new-dataset-first_domain.zip",
+#     "second_domain": "https://huggingface.co/great-new-dataset-second_domain.zip",
+# }
 
 
-# TODO: Name of the dataset usually match the script name with CamelCase instead of snake_case
 class MIMICDataset(datasets.GeneratorBasedBuilder):
     """MIMIC-III dataset"""
 
     VERSION = datasets.Version("1.4.0")
 
-    # This is an example of a dataset with multiple configurations.
-    # If you don't want/need to define several sub-sets in your dataset,
-    # just remove the BUILDER_CONFIG_CLASS and the BUILDER_CONFIGS attributes.
-
-    # If you need to make complex sub-parts in the datasets with configurable options
-    # You can create your own builder configuration class to store attribute, inheriting from datasets.BuilderConfig
-    # BUILDER_CONFIG_CLASS = MyBuilderConfig
-
-    # You will be able to load one or the other configurations in the following list with
-    # data = datasets.load_dataset('my_dataset', 'first_domain')
-    # data = datasets.load_dataset('my_dataset', 'second_domain')
+    # This is a list of multiple configurations for these datasets.
     BUILDER_CONFIGS = [
         datasets.BuilderConfig(name="minimum", version=VERSION, description="This is the bare minimum dataset"),    
     ]
 
-    DEFAULT_CONFIG_NAME = "minimum"  # It's not mandatory to have a default configuration. Just use one if it make sense.
+    #This is the default configuration
+    DEFAULT_CONFIG_NAME = "minimum"
 
     def _info(self):
-        # TODO: This method specifies the datasets.DatasetInfo object which contains informations and typings for the dataset
+        # This method specifies the datasets.DatasetInfo object which contains informations and typings for the dataset
         if self.config.name == "minimum":  # This is the name of the configuration selected in BUILDER_CONFIGS above
+            # These are the features of your dataset
             features = datasets.Features(
                 {
                     "DOB": datasets.Value("string"),
@@ -94,10 +80,9 @@ class MIMICDataset(datasets.GeneratorBasedBuilder):
                     "ADMITTIME": datasets.Value("string"),
                     "DISCHTIME": datasets.Value("string"),
                     "TEXT": datasets.Value("string"),
-                    # These are the features of your dataset like images, labels ...
                 }
             )
-        # else:  # This is an example to show how to have different features for "first_domain" and "second_domain"
+        # else: 
         #     features = datasets.Features(
         #         {
         #             "DOB": datasets.Value("string"),
@@ -105,17 +90,13 @@ class MIMICDataset(datasets.GeneratorBasedBuilder):
         #             "ADMITTIME": datasets.Value("string"),
         #             "DISCHTIME": datasets.Value("string"),
         #             "TEXT": datasets.Value("string"),
-        #             # These are the features of your dataset like images, labels ...
         #         }
         #    )
         return datasets.DatasetInfo(
             # This is the description that will appear on the datasets page.
             description=_DESCRIPTION,
             # This defines the different columns of the dataset and their types
-            features=features,  # Here we define them above because they are different between the two configurations
-            # If there's a common (input, target) tuple from the features, uncomment supervised_keys line below and
-            # specify them. They'll be used if as_supervised=True in builder.as_dataset.
-            # supervised_keys=("sentence", "label"),
+            features=features,
             # Homepage of the dataset for documentation
             homepage=_HOMEPAGE,
             # License for the dataset if available
@@ -125,12 +106,14 @@ class MIMICDataset(datasets.GeneratorBasedBuilder):
         )
 
     def _split_generators(self, dl_manager):
-        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
+        # This method is tasked with defining the splits depending on the configuration
         # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
 
+        # the original template of this method is also tasked with downloading and extracting data from URLs. 
+        # However, since we work with our data locally, we don't need this functionality. 
         # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLS
-        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
-        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
+        # I still keep this parameter for any later potential use.
+
         # urls = _URLS[self.config.name]
         # data_dir = dl_manager.download_and_extract(urls)
 
@@ -164,7 +147,7 @@ class MIMICDataset(datasets.GeneratorBasedBuilder):
 
     # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
     def _generate_examples(self, filepath, split):
-        # TODO: This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
+        # This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
         # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
         with open(filepath, encoding="utf-8") as f:
             csvreader = csv.reader(f, delimiter=",")
--- text2table/data/dataset_test.py
+++
@@ -1,5 +0,0 @@
-import datasets
-
-mimic = datasets.load_dataset("./dataset_loading_script.py", split = "train")
-print(mimic)
-print(mimic[0])
