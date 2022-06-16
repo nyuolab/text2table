8b8b0e81cecc3e3976b879607891d07e1ef6db77
Zhang <cz1906@bigpurple-ln1.cm.cluster>
2022-06-16 19:04:05 -0400

    mvp-model-plus-pretokenizer

--- text2table/models/train_model.py
+++ text2table/models/train_model.py
@@ -3,11 +3,12 @@
 import datasets
 from transformers import (LEDTokenizerFast, LEDForConditionalGeneration, Seq2SeqTrainingArguments, Seq2SeqTrainer)
 import os, shutil, logging
+from tokenizer import tokenize
 
 
 # Specify the directory where the pretokenized data are stored: train & validation sets
-ckpt_dir_train = '/gpfs/data/oermannlab/users/cz1906/text2table/text2table/data/pretokenized/train/'
-ckpt_dir_val = '/gpfs/data/oermannlab/users/cz1906/text2table/text2table/data/pretokenized/val/'
+ckpt_dir_train = '../data/pretokenized/train/'
+ckpt_dir_val = '../data/pretokenized/val/'
 
 
 # Load tokenizer for the LED model
@@ -18,113 +19,36 @@ tokenizer.add_special_tokens({"additional_special_tokens": ["<COL>", "<ROW>", "<
 
 
 # If the pretokenized data are exists, load it directly from the disk (time-saving)
-# If not, tokenized the text for model and store it for faster reuse
+# If not, tokenized the text for model and store it for faster reuse (Call Tokenizer in the same directory)
 if os.path.exists(ckpt_dir_train) and os.path.exists(ckpt_dir_val):
 
     # Load the pre-tokenzied training dataset
     train_dataset = datasets.load_from_disk(ckpt_dir_train)
+    
     # Load the pre-tokenized validation dataset
     val_dataset = datasets.load_from_disk(ckpt_dir_val)
 
 else:
+    # Pre-tokenize the input text
+    tokenize()
 
-    # Load preprocessed data: with special tokens added
-    train_dataset = datasets.load_dataset('../data/dataset_loading_script.py', split='train')
-    val_dataset = datasets.load_dataset('../data/dataset_loading_script.py', split='validation')
-
-
-    # Define the maximum input and output sequence length
-    max_input_length = 8192
-    max_output_length = 512 
-    # Define the processing function so that the data will match the correct model format
-    def process_data_to_model_inputs(batch):
-        # Tokenize the input text
-        inputs = tokenizer(
-            batch['TEXT'],
-            padding='max_length',
-            truncation=True,
-            max_length=max_input_length,
-        )
-        # Tokenize the output text
-        outputs = tokenizer(
-            batch['TABLE'],
-            padding='max_length',
-            truncation=True,
-            max_length=max_output_length,
-        )
-        # Tokenize the head column for decoder
-        head = tokenizer(
-            batch['HEADER'],
-        )
-
-        # Assign the input IDs and corresponding attention mask
-        batch["input_ids"] = inputs.input_ids
-        batch["attention_mask"] = inputs.attention_mask
-        # Assign the header IDs
-        batch["decoder_input_ids"] = head.input_ids
-
-        # create 0 global_attention_mask lists (0: token attends 'locally')
-        batch["global_attention_mask"] = len(batch["input_ids"]) * [
-            [0 for _ in range(len(batch["input_ids"][0]))]
-        ]
-        # Change the first token of all sequences to make it attend 'globally' (suggested by the original paper)
-        batch["global_attention_mask"][0][0] = 1
-
-        # Assign the output IDs
-        batch["labels"] = outputs.input_ids
-
-        # Ignore the PAD token
-        batch["labels"] = [
-            [-100 if token == tokenizer.pad_token_id else token for token in labels]
-            for labels in batch["labels"]
-        ]
-        
-        return batch
+    # Load the pre-tokenzied training dataset
+    train_dataset = datasets.load_from_disk(ckpt_dir_train)
+    
+    # Load the pre-tokenized validation dataset
+    val_dataset = datasets.load_from_disk(ckpt_dir_val)
 
 
-    # Define the batch size & Num of CPUs
-    batch_size = 32
-    num_cpu = 32
-    # Preprocess(Tokenize) the input data: Training set
-    train_dataset = train_dataset.map(
-        function=process_data_to_model_inputs,
-        batched=True,
-        batch_size=batch_size,
-        num_proc=num_cpu,
-        remove_columns=["DOB", "SEX", "ADMITTIME", "ICD9"],
-    )
-    # Preprocess(Tokenize) the input data: Validation set
-    val_dataset = val_dataset.map(
-        function=process_data_to_model_inputs,
-        batched=True,
-        batch_size=batch_size,
-        num_proc=num_cpu,
-        remove_columns=["DOB", "SEX", "ADMITTIME", "ICD9"],
-    )
 
-    # Convert the dataset to Pytorch format for LED
-    train_dataset.set_format(
-        type="torch",
-        columns=["input_ids", "attention_mask", "global_attention_mask", "labels"],
-    )
-    val_dataset.set_format(
-        type="torch",
-        columns=["input_ids", "attention_mask", "global_attention_mask", "labels"],
-    )
-
-    # Creat a directory to store the pretokenized data for training set
-    os.makedirs(ckpt_dir_train, exist_ok=True)
-    # Save
-    train_dataset.save_to_disk(ckpt_dir_train)
-    logging.info(f'saved tokenized training dataset to {ckpt_dir_train}')
-
-    # Creat a directory to store the pretokenized data for validation set
-    os.makedirs(ckpt_dir_val, exist_ok=True)
-    # Save
-    val_dataset.save_to_disk(ckpt_dir_val)
-    logging.info(f'saved tokenized validation dataset to {ckpt_dir_val}')
-
-
+# Convert the dataset to Pytorch format for LED
+train_dataset.set_format(
+    type="torch",
+    columns=["input_ids", "attention_mask", "global_attention_mask", "labels"],
+)
+val_dataset.set_format(
+    type="torch",
+    columns=["input_ids", "attention_mask", "global_attention_mask", "labels"],
+)
 
 # Initialize the model
 model = LEDForConditionalGeneration.from_pretrained("allenai/led-base-16384", gradient_checkpointing=True)
@@ -146,9 +70,9 @@ training_args = Seq2SeqTrainingArguments(
     per_device_train_batch_size=2,
     per_device_eval_batch_size=2,
     fp16=True,
-    logging_steps=5,
-    eval_steps=600,
-    save_steps=600,
+    logging_steps=10,
+    eval_steps=1000,
+    save_steps=1000,
     save_total_limit=2,
     gradient_accumulation_steps=4,
 )
