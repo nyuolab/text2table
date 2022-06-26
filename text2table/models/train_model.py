a08e7c1ad908a92099b0495aeedb604e37bfc824
Wu <wul20@gn-0002.cm.cluster>
2022-06-26 18:26:25 -0400

    changed train_model.py's 'mode' arg

--- text2table/models/train_model.py
+++ text2table/models/train_model.py
@@ -32,7 +32,7 @@ train_dataset = datasets.load_from_disk(ckpt_dir_train)
 # Load the pre-tokenized validation dataset
 val_dataset = datasets.load_from_disk(ckpt_dir_val)
 
-# Convert the dataset to Pytorch format for LED
+
 train_dataset.set_format(
     type="torch",
     columns=["input_ids", "attention_mask", "global_attention_mask", "labels"],
@@ -41,6 +41,8 @@ val_dataset.set_format(
     type="torch",
     columns=["input_ids", "attention_mask", "global_attention_mask", "labels"],
 )
+
+
 print('shape: ',val_dataset.shape)
 
 #--changed,resume
@@ -48,15 +50,13 @@ val_dataset=val_dataset.select(range(10))
 print('\nafter slicing: ')
 print('shape: ',val_dataset.shape)
 
-
 # Modify model & trainer parameters
 gradient_checkpointing=True
 
 predict_with_generate=True
 evaluation_strategy="steps"
-per_device_train_batch_size=2
-per_device_eval_batch_size=2
-
+per_device_train_batch_size=1
+per_device_eval_batch_size=1
 
 
 # Initialize the model
@@ -73,7 +73,7 @@ model.config.length_penalty = 1.0
 model.config.early_stopping = True
 
 
-# Declare the training arguments
+# Declare the training pts
 training_args = Seq2SeqTrainingArguments(
     output_dir="../../models/",
     predict_with_generate=predict_with_generate,
@@ -92,9 +92,9 @@ training_args = Seq2SeqTrainingArguments(
 )
 
 
-#--changed: load custom metrics
 
-cel_match = load_metric('new_metric_script.py', config_name='0')
+#--changed: load custom metrics
+cel_match = load_metric('new_metric_script.py')
 # Define the metric function for evalutation
 def compute_metrics(pred):
     # Prediction IDs
@@ -107,30 +107,10 @@ def compute_metrics(pred):
     label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=False)
     
     #cel_match.add_batch(predictions=pred_str, references=label_str)
-    cel_match = load_metric('new_metric_script.py', config_name='0')
-    cel_match_output_0 = cel_match.compute(predictions=pred_str,references=label_str)
 
-    cel_match = load_metric('new_metric_script.py', config_name='10')
-    cel_match_output_10 = cel_match.compute(predictions=pred_str,references=label_str)
-
-    cel_match = load_metric('new_metric_script.py', config_name='20')
-    cel_match_output_20 = cel_match.compute(predictions=pred_str,references=label_str)
+    # Compute the rouge evaluation results
+    cel_match_output = cel_match.compute(predictions=pred_str,references=label_str,mode=['0','10','20'])
     
-    exit(0)
-
-    final={}
-    
-   
-
-    # Return the results
-    # return {
-    #     cel_match_output
-    # }
-    #--changed
-
-    
-
-
     return cel_match_output
 
 # Initialize the trainer
@@ -140,7 +120,7 @@ trainer = Seq2SeqTrainer(
     args=training_args,
     compute_metrics=compute_metrics,
     train_dataset=train_dataset,
-    eval_dataset=val_dataset,
+    eval_dataset=val_dataset
 )
 
 # Start the training
